4de2a3668f5d80a0ca33f926fd50c2311ce4ed3a
Eddie McConkie <edwardmcconkie@gmail.com>
2022-01-15 16:15:19 -0700

    Added week 2 & 3 notes and styling

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 const links = [
   {
-    label: 'Week 01 Notes',
+    label: 'Week 01',
     url: 'week01',
+  },
+  {
+    label: 'Week 02',
+    url: 'week02',
+  },
+  {
+    label: 'Week 03',
+    url: 'week03',
   },
 ]
 
 const ol = document.querySelector('ol')
-ol.innerHTML = links.map((link) => `<li><a href="${link.url}">${link.label}</a></li>`)
+ol.innerHTML = links.map((link) => `<li><a href="${link.url}">${link.label}</a></li>`).join('')
